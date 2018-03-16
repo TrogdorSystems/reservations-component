6e8f464da747ff10ea2372374a7d723973a921a3
Michael Durfey <mdurfey@me.com>
2018-03-16 16:03:45 -0700

    queries optimized, script added to add index after seeding

--- server/db/copySeed.sql
+++ server/db/copySeed.sql
@@ -2,3 +2,4 @@
 
 \copy restaurants FROM 'restaurant.csv' WITH csv;
 \copy reservations (restaurantid, date, time, name, party) FROM 'reservation.csv' WITH csv;
+CREATE INDEX reservations_restaurantid_index on reservations (restaurantid);
