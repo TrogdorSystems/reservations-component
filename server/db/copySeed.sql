269edfa3636324700b2e28cbdd5aaef8b7e1a0c2
Michael Durfey <mdurfey@me.com>
2018-03-16 19:24:14 -0700

    fixed tests

--- server/db/copySeed.sql
+++ server/db/copySeed.sql
@@ -3,3 +3,5 @@
 \copy restaurants FROM 'restaurant.csv' WITH csv;
 \copy reservations (restaurantid, date, time, name, party) FROM 'reservation.csv' WITH csv;
 CREATE INDEX reservations_restaurantid_index on reservations (restaurantid);
+alter table reservations add foreign key(restaurantId) references restaurants (id) on delete cascade on update cascade;
+alter table restaurants add primary key (id);
